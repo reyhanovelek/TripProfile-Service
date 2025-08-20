61708597659af60a669e17cf6cb10a95eccee03e
reyhan ovelek <reyhanovelek@gmail.com>
2025-08-20 13:58:14 +0200

    Initial commit - TripProfile microservice setup with application.yml

---
+++ tripprofile/src/main/java/com/tripwise/tripprofile/TripprofileApplication.java
@@ -0,0 +1,13 @@
+package com.tripwise.tripprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripprofileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripprofileApplication.class, args);
+	}
+
+}
---
+++ tripprofile/src/test/java/com/tripwise/tripprofile/TripprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripwise.tripprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
